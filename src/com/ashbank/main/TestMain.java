2d181c5f4bc8889cba7bdd10dd3225a7133d03ab
Emmanuel Enchill <mzigah4@gmail.com>
2025-02-12 15:13:07 +0000

    Button Control Integrity 1. Updated the code for the Save button to enforce the user to select an option by displaying an error message before proceeding to save a record.
    2. Updated the code for the Cancel button to switch to dashboard scene

--- src/com/ashbank/main/TestMain.java
+++ src/com/ashbank/main/TestMain.java
@@ -1,13 +1,9 @@
 package com.ashbank.main;
 
-import com.ashbank.db.db.engines.ActivityLoggerStorageEngine;
-import com.ashbank.objects.scenes.auth.UserAuthScenes;
 import com.ashbank.objects.utility.SceneController;
 import com.ashbank.db.db.InitializePlatform;
 
 import javafx.application.Application;
-import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.sql.SQLException;
@@ -18,14 +14,11 @@ public class TestMain extends Application{
     public void start(Stage primaryStage) throws SQLException {
         InitializePlatform.initiateSystem();
 
-
         SceneController sceneController = new SceneController(primaryStage);
         sceneController.showUserAuthScene();
         primaryStage.setResizable(true);
         primaryStage.setMaximized(true);
         primaryStage.show();
-
-//        UserAuthScenes userAuthScenes = new UserAuthScenes(primaryStage);
 
 //        String osName = System.getProperty("os.name");
 //        String osUsername = System.getProperty("user.name");
@@ -39,11 +32,6 @@ public class TestMain extends Application{
 //            throw new RuntimeException(ex);
 //        }
 
-//        primaryStage.getIcons().add(new Image("/com/ashbank/objects/resources/icons/bank.png"));
-//        primaryStage.setMaximized(true);
-//        primaryStage.setResizable(true);
-//        userAuthScenes.getUserLoginScene();
-//        primaryStage.show();
     }
 
     public static void main(String[] args) {
