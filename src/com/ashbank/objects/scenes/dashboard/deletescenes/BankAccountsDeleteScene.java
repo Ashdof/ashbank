14103fb528704427ee7af85748e5a2242283ccf3
Emmanuel Enchill <mzigah4@gmail.com>
2025-03-03 07:19:26 +0000

    Implemented methods to display a table of transactions associated with the bank account object

--- src/com/ashbank/objects/scenes/dashboard/deletescenes/BankAccountsDeleteScene.java
+++ src/com/ashbank/objects/scenes/dashboard/deletescenes/BankAccountsDeleteScene.java
@@ -6,32 +6,40 @@ package com.ashbank.objects.scenes.dashboard.deletescenes;
  * @author Emmanuel Amoaful Enchill
  */
 
+import com.ashbank.db.db.engines.BankAccountsStorageEngine;
 import com.ashbank.db.db.engines.CustomersStorageEngine;
 import com.ashbank.objects.bank.BankAccounts;
 import com.ashbank.objects.utility.CustomDialogs;
 import com.ashbank.objects.utility.SceneController;
+
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Separator;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
+import java.sql.Date;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class BankAccountsDeleteScene {
 
     /* ================ DATA MEMBERS ================ */
-    private static final Logger logger = Logger.getLogger(TransactionDeleteScene.class.getName());
+    private static final Logger logger = Logger.getLogger(BankAccountsDeleteScene.class.getName());
     private Scene bankAccountsDeleteScene;
+    private List<BankAccounts> bankAccountsList;
+    private ObservableList<BankAccounts> bankAccountsObservableList;
+    private TableView<BankAccounts> bankAccountsTableView = new TableView<>();
+    private String customerID, accountID;
 
     private static final CustomDialogs customDialogs = new CustomDialogs();
     private final SceneController sceneController;
@@ -141,6 +149,140 @@ public class BankAccountsDeleteScene {
         vBoxRoot.getChildren().addAll(hBoxWarning, gridPane);
 
         return vBoxRoot;
+    }
+
+    /**
+     * Table of Bank Account Objects:
+     * create a vertical vox object containing a table list of all
+     * bank account objects
+     * @return the VBox object
+     */
+    private VBox getListOfAllBankAccounts() {
+
+        TextField txtSearch;
+        VBox vBox;
+
+        bankAccountsTableView = new TableView<>();
+        bankAccountsTableView.setMinWidth(1000);
+        bankAccountsTableView.setMinHeight(600);
+        this.initializeBankAccountDataTable();
+
+        bankAccountsList = BankAccountsStorageEngine.getAllBankAccountData();
+        bankAccountsObservableList = FXCollections.observableArrayList(bankAccountsList);
+        bankAccountsTableView.setItems(bankAccountsObservableList);
+        bankAccountsTableView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                accountID = newValue.getAccountID();
+            }
+        });
+        bankAccountsTableView.setOnMouseClicked(e -> {
+            if (e.getClickCount() == 2 && !bankAccountsTableView.getSelectionModel().isEmpty()) {
+                BankAccounts bankAccounts = bankAccountsTableView.getSelectionModel().getSelectedItem();
+                customerID = bankAccounts.getCustomerID();
+                accountID = bankAccounts.getAccountID();
+
+                try {
+                    sceneController.showBankAccountDetailsScene(accountID);
+                } catch (SQLException sqlException) {
+                    throw  new RuntimeException();
+                }
+            }
+        });
+
+        txtSearch = new TextField();
+        txtSearch.setPromptText("Search ...");
+        txtSearch.textProperty().addListener((observable, oldValue, newValue) -> {
+            this.filterBankAccounts(newValue);
+        });
+
+        vBox = new VBox(10);
+        vBox.setPadding(new Insets(10));
+        vBox.setAlignment(Pos.TOP_LEFT);
+        vBox.getChildren().addAll(txtSearch, bankAccountsTableView);
+
+        return vBox;
+    }
+
+    /**
+     * Customers Basic Data Table:
+     * initialize a table with the basic data of customers
+     */
+    private void initializeBankAccountDataTable() {
+
+        TableColumn<BankAccounts, String> accountNumber, accountType, accountCurrency, dateCreated, accountStatus,
+                customerName;
+        TableColumn<BankAccounts, Double> accountBalance, initialDeposit;
+        TableColumn<BankAccounts, Number> numberTableColumn;
+        TableColumn<BankAccounts, Date> lastTransactionDate;
+
+        numberTableColumn = new TableColumn<>("#");
+        numberTableColumn.setMinWidth(50);
+        numberTableColumn.setCellValueFactory(data ->
+                new ReadOnlyObjectWrapper<>(bankAccountsTableView.getItems().indexOf(data.getValue()) + 1)
+        );
+        numberTableColumn.setSortable(false); // Disable sorting for numbering of columns
+        numberTableColumn.setStyle("-fx-alignment: CENTER;");
+
+        accountNumber = new TableColumn<>("Account Number");
+        accountNumber.setCellValueFactory(data -> new ReadOnlyObjectWrapper<>(data.getValue().getAccountNumber()));
+
+        accountType = new TableColumn<>("Account Type");
+        accountType.setCellValueFactory(data -> new ReadOnlyObjectWrapper<>(data.getValue().getAccountType()));
+
+        accountCurrency = new TableColumn<>("Currency");
+        accountCurrency.setCellValueFactory(data -> new ReadOnlyObjectWrapper<>(data.getValue().getAccountCurrency()));
+
+        dateCreated = new TableColumn<>("Date Created");
+        dateCreated.setCellValueFactory(data -> new ReadOnlyObjectWrapper<>(data.getValue().getDateCreated()));
+
+        accountBalance = new TableColumn<>("Current Balance");
+        accountBalance.setCellValueFactory(data -> new ReadOnlyObjectWrapper<>(data.getValue().getAccountBalance()));
+
+        initialDeposit = new TableColumn<>("Initial Deposit");
+        initialDeposit.setCellValueFactory(data -> new ReadOnlyObjectWrapper<>(data.getValue().getInitialDeposit()));
+
+        lastTransactionDate = new TableColumn<>("Last Transaction Date");
+        lastTransactionDate.setCellValueFactory(data -> new ReadOnlyObjectWrapper<>(data.getValue().getLastTransactionDate()));
+
+        accountStatus = new TableColumn<>("Status");
+        accountStatus.setCellValueFactory(data -> new ReadOnlyObjectWrapper<>(data.getValue().getAccountStatus()));
+
+        customerName = new TableColumn<>("Account Owner");
+        customerName.setCellValueFactory(data -> {
+            try {
+                return new ReadOnlyObjectWrapper<>(new CustomersStorageEngine().getCustomerDataByID(data.getValue().getCustomerID()).getFullName());
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        bankAccountsTableView.getColumns().addAll(numberTableColumn, accountNumber, accountType, accountCurrency, dateCreated, initialDeposit, accountBalance, lastTransactionDate, accountStatus, customerName);
+    }
+
+    /**
+     * Filter Bank Account Objects:
+     * filters a list of bank account objects according to a
+     * search query
+     * @param query the search query
+     */
+    private void filterBankAccounts(String query) {
+
+        if (query == null || query.isEmpty()) {
+            bankAccountsTableView.setItems(bankAccountsObservableList);
+        } else {
+            ObservableList<BankAccounts> filteredList = FXCollections.observableArrayList();
+
+            for (BankAccounts bankAccounts : bankAccountsObservableList) {
+
+                if (bankAccounts.getAccountNumber().contains(query.toLowerCase()) ||
+                        bankAccounts.getAccountType().toLowerCase().contains(query.toLowerCase()) ||
+                        bankAccounts.getAccountCurrency().toLowerCase().contains(query.toLowerCase())) {
+                    filteredList.add(bankAccounts);
+                }
+            }
+
+            bankAccountsTableView.setItems(filteredList);
+        }
     }
 
     /**
